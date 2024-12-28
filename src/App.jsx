68e9061d366292098ed7c4ba7809fe0c3f353d44
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-12-28 11:21:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         
       </Routes>
